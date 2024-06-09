1a7a5eb709a45fa669ca002ecc84bbe5bdc6bb35
Alejandro Alzate <alejoalzate12@gmail.com>
2024-06-08 19:56:43 -0500

    Configure routes to support all CRUD actions for Post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  resources :posts
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
